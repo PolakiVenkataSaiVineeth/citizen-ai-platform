1d48e70576afd131a5f2df3345d09b2258f2ab0f
vinnybolt <vinnyb6426@gmail.com>
2025-06-19 19:25:59 +0530

    Delete nlu_handler.py

--- nlu_handler.py
+++
@@ -1,8 +0,0 @@
-def process_nlu(text):  # Changed from process_text to process_nlu
-    # Implement NLU processing here
-    # This is a placeholder - you'll need to add your actual NLU processing logic
-    return {
-        'entities': [],
-        'keywords': [],
-        'categories': []
-    }
